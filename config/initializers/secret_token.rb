47ee278282edf1d548a3ee56b9d948dd66937f73
Quido Hoekman <quido@qhoekman.nl>
2012-11-21 10:18:56 +0100

    Basis rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TedxIteratie3::Application.config.secret_token = 'd4cc2ac2456d9142ea5741ae41154f69a22293ce6547a7e3252b184642dede6847318f0a446b208833e76ed5e674ec4062d8bcec27b41ea49855d594d0c8d2dd'
